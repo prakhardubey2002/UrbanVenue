9404a60f031afe0484bb479b9a4b4cf40a2948d5
Prakhar Dubey <prakhardubey2002@gmail.com>
2024-09-10 14:38:31 +0530

    Invoive Ui updated

--- src/pages/Invoice.jsx
+++ src/pages/Invoice.jsx
@@ -5,6 +5,16 @@ import { usePDF } from 'react-to-pdf'
 import invoice from '../assets/invoice.png'
 import qr from '../assets/qr.png'
 import { DASHBOARD_ROUTE } from '../routes/Routes'
+import CloudDownloadIcon from '@mui/icons-material/CloudDownload'
+import HomeIcon from '@mui/icons-material/Home'
+import EmailIcon from '@mui/icons-material/Email'
+import LanguageIcon from '@mui/icons-material/Language'
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import PinterestIcon from '@mui/icons-material/Pinterest';
 const Invoice = () => {
   const navigate = useNavigate()
   const location = useLocation()
@@ -19,31 +29,41 @@ const Invoice = () => {
         ref={targetRef}
         className="flex flex-col w-[70%] border border-black"
       >
-        <div className="px-4 bg-Primary h-[14vh] flex justify-between items-center">
-          <img src={Logo} alt="" />
-          <button onClick={() => toPDF()} className="text-white">
-            Download
-          </button>
+        <div className="relative h-[14vh] flex">
+          <div className="w-1/3 bg-black flex items-center justify-center z-0">
+            <span className="text-white"> </span>
+          </div>
+          <div className="w-1/3 bg-[#ce2a33] flex items-center justify-center z-0">
+            <span className="text-white"> </span>
+          </div>
+          <div className="w-1/3 bg-black flex items-center justify-center z-0">
+            <span className="text-white"> </span>
+          </div>
+          <div className="absolute right-[1%] top-[45%] flex justify-between items-center px-4 z-10">
+            <button onClick={() => toPDF()} className="text-white flex  ">
+              <CloudDownloadIcon className="mx-2" />
+              Download
+            </button>
+          </div>
         </div>
         <div className="m-4">
+          <img src={Logo} className="w-24 my-4 h-auto" alt="Logo" />
           <h2 className="font-semibold text-2xl">Invoice</h2>
           <div className="flex flex-col items-start">
-            <div className="w-[90%] flex justify-between">
+            <div className="w-full flex justify-between">
               <p>Invoice to {formData.guestName}</p>
               <p>Invoice ID: {formData.bookingId}</p>
             </div>
-            <div className="w-[90%] flex justify-between">
+            <div className="w-full flex justify-between">
               <p>{formData.phoneNumber}</p>
               <p>Order Date: {formData.checkInDate}</p>
             </div>
-            <div className="w-[90%] flex justify-between">
+            <div className="w-full flex justify-between">
               <p> </p>
               <p>{formData.venue}</p>
             </div>
-            <div className="w-[90%] flex justify-between">
-              <p>
-                
-              </p>
+            <div className="w-full flex justify-between">
+              <p></p>
               <p>{formData.addressLine1}</p>
             </div>
           </div>
@@ -144,7 +164,7 @@ const Invoice = () => {
           </h2>
           <p>Paid via {formData.advanceMode}</p>
         </div>
-        {formData.showAdvanceDetails ==="yes" && (
+        {formData.showAdvanceDetails === 'yes' && (
           <div className="p-4">
             <table className="min-w-full bg-white border border-gray-300">
               <thead>
@@ -166,10 +186,10 @@ const Invoice = () => {
                     Advance Breakup
                   </td>
                   <td className="py-2 px-4 border-b text-left">
-                  ₹  {formData.farmTref}
+                    ₹ {formData.farmTref}
                   </td>
                   <td className="py-2 px-4 border-b text-left">
-                  ₹ {formData.otherServices}
+                    ₹ {formData.otherServices}
                   </td>
                   <td className="py-2 px-4 border-b text-left">
                     ₹ {formData.urbanvenuecommission}
@@ -187,34 +207,33 @@ const Invoice = () => {
         )}
 
         <div>
-          <div className="  flex items-center justify-between bg-white border-t border-gray-300">
+          {/* <div className="  flex items-center justify-between bg-white border-t border-gray-300">
             <div className="flex-1 flex items-center p-8">
               <img
-                src={invoice} // Replace with the correct field name
-                alt="invoice" // Replace with the correct field name
+                src={invoice}
+                alt="invoice" 
                 className="w-[100px] h-[100px] object-cover rounded-lg"
               />
               <div className="ml-4">
                 <h4 className="font-semibold">{formData.occasion}</h4>{' '}
-                {/* Event Name */}
+                
                 <p className="text-sm text-gray-500">
                   {new Date(formData.checkInDate).toLocaleDateString()}{' '}
                   {new Date(formData.checkOutDate).toLocaleTimeString()}.
                   Duration{' '}
-                  {/* {calculateDuration(formData.checkIn, formData.checkOut)}{' '} */}
-                  {/* Calculated Duration */}
+                 
                 </p>
                 <p className="text-sm text-gray-500">{formData.guestName}</p>{' '}
-                {/* Organizer Name */}
+                
                 <p className="text-sm text-gray-500">1 x Ticket</p>{' '}
-                {/* Ticket Count */}
+              
                 <p className="font-semibold">
                   Total : ₹{' '}
                   {Number(formData.balancePayment) +
                     Number(formData.advance) +
                     Number(formData.securityAmount)}
                 </p>{' '}
-                {/* Total Amount */}
+               
               </div>
             </div>
             <div className="flex-1 p-4 bg-Bordgrey flex flex-col items-start">
@@ -228,13 +247,79 @@ const Invoice = () => {
                 Powered by Urban Venue
               </p>
             </div>
+          </div> */}
+          <div className="relative h-[14vh] my-2 flex">
+            <div className="w-1/3 relative bg-black flex  items-center justify-center z-0">
+              <div className="absolute bg-[#ce2a33]  border border-white p-2 top-[-10%] rounded-3xl ">
+                <LocalPhoneIcon className=" text-white" />
+              </div>
+              <a href="tel:+919871371364" className="text-white ">
+                +91 9871371364
+              </a>
+            </div>
+            <div className="w-1/3 bg-[#ce2a33] relative flex items-center justify-center z-0">
+              <div className="absolute bg-[#ce2a33]  border border-white p-2 top-[-10%] rounded-3xl ">
+                <EmailIcon className="text-white" />
+              </div>
+              <a href="mailto:TheUrbanVenue@gmail.com" className="text-white ">
+                TheUrbanVenue@gmail.com
+              </a>
+            </div>
+            <div className="w-1/3 relative bg-black flex items-center justify-center z-0">
+              <div className="absolute bg-[#ce2a33]  border border-white p-2 top-[-10%] rounded-3xl ">
+                <LanguageIcon className="text-white" />
+              </div>
+              <a
+                href="https://www.urbanvenue.in"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white underline-none"
+              >
+                www.urbanvenue.in
+              </a>
+            </div>
           </div>
         </div>
-        <div className=" my-8 flex justify-center items-center">
-          <button onClick={() => Home()} className="button ">
-            Home
-          </button>
+        <div className="relative h-[7vh] flex flex-wrap">
+          <div className="w-1/5 bg-black flex items-center justify-center z-0">
+            <span className="text-white"> 
+            <InstagramIcon/>
+            @theurbanvenue
+            </span>
+          </div>
+          <div className="w-1/5 bg-[#ce2a33] flex items-center justify-center z-0">
+            <span className="text-white">
+              <FacebookIcon/>
+            @theurbanvenue
+               </span>
+          </div>
+          <div className="w-1/5 bg-black flex items-center justify-center z-0">
+            <span className="text-white"> 
+            <YouTubeIcon/>
+            @theurbanvenue
+            </span>
+          </div>
+          <div className="w-1/5 bg-[#ce2a33] flex items-center justify-center z-0">
+            <span className="text-white"> 
+            <LinkedInIcon />
+            @theurbanvenue
+            </span>
+          </div>
+          <div className="w-1/5 bg-black flex items-center justify-center z-0">
+            <span className="text-white"> 
+            <PinterestIcon/>
+            @theurbanvenue
+            </span>
+          </div>
         </div>
+      </div>
+      <div
+        onClick={() => Home()}
+        className=" cursor-pointer bg-Primary my-10 ml-2 p-2 flex justify-center items-center rounded "
+      >
+        {/* <button  className="button "> */}
+        <HomeIcon className="text-white" />
+        {/* </button> */}
       </div>
     </div>
   )
